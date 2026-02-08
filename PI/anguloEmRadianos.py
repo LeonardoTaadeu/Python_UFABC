917d9edf517db3ea9f86347dbf1fb719f82a488f
Leonardo <leonardo.pedrobr@gmail.com>
2026-02-08 16:03:00 -0300

    angulos em radianosss

---
+++ PI/anguloEmRadianos.py
@@ -0,0 +1,9 @@
+import math
+
+angulo = float(input())
+
+primeiraConta = math.pi*180
+
+radiano = angulo*primeiraConta
+
+print(f"{angulo:.2f} graus correspondem a {radiano:.2f} radianos")
---
+++ PI/trocarPalavras.py
@@ -0,0 +1,7 @@
+texto = input("")
+palavraParaTrocar = input("")
+novaPalavra = input("")
+
+textoModificado = texto.replace(palavraParaTrocar, novaPalavra)
+
+print(textoModificado)
