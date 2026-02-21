3133dad4ac6aa8b3926fddc8abbf94f3706c93dd
Leonardo <leonardo.pedrobr@gmail.com>
2026-02-21 10:54:32 -0300

    exercicio de horas e polegadas

--- PI/horaMinutosSegundos.py
+++ PI/horaMinutosSegundos.py
@@ -2,5 +2,12 @@ totalSegundos = int(input("Digite um numero total de segundos: "))
 
 horas = totalSegundos//3600
 
+resto = totalSegundos%3600
+
+minutos = resto//60
+
+segundos = resto%60
 
 print(horas)
+
+print("{} segundos correspondem a {:02d}:{:02d}:{:02d}".format(totalSegundos, horas, minutos, segundos))
