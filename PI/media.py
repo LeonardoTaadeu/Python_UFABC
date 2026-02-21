a09c698155578a4e7eac1a69fd696b9d06c22098
Leonardo <leonardo.pedrobr@gmail.com>
2026-02-21 11:07:05 -0300

    Media com funcao

---
+++ PI/media.py
@@ -0,0 +1,10 @@
+def media(a, b):
+    return (a + b) / 2
+
+num1 = float(input("Digite um numero: "))
+
+num2 = float(input("Digite outro numero: "))
+
+resultado = media(num1, num2)
+
+print("A media dos dois numeros e {}".format(resultado))
