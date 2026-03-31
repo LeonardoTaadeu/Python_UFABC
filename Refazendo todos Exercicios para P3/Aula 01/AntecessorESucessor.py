e6c36226f3608812c7114420b1efd0af91b40128
Leonardo <leonardo.pedrobr@gmail.com>
2026-03-30 22:43:53 -0300

    antecessor e sucessor

---
+++ Refazendo todos Exercicios para P3/Aula 01/AntecessorESucessor.py
@@ -0,0 +1,8 @@
+num1 = int(input())
+
+antecessor = num1 - 1
+sucessor = num1 + 1
+
+print("Você digitou o número {}".format(num1))
+print("O antecessor de {} é {}".format(num1, antecessor))
+print("O sucessor de {} é {}".format(num1, sucessor))
